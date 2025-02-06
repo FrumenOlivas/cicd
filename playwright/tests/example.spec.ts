d3651c1b8819ac8a7c3f12aab1c95d9e71300024
Frumen Olivas <frumen@olivas.com>
2025-02-05 20:32:12 -0600

    add playwright

---
+++ playwright/tests/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("create a post", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("textbox", { name: "Title" }).click();
+  await page.getByRole("textbox", { name: "Title" }).fill("Hola que hace");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByText("Your most recent post: Hola").click();
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
+});
